c38f60a9dc4a35e52401f157a5d285d5258e96c9
Robert L. Carpenter <robacarp@gmail.com>
2013-04-26 12:13:49 -0600

    Patch up a better working rvm for fish.
    - now compatible with .ruby-version :)

--- .config/fish/hooks/rvm.fish
+++ .config/fish/hooks/rvm.fish
@@ -1,23 +1,28 @@
+#!/usr/bin/env fish
+
 function __check_rvm_at_cwd --on-event cwd
   # Source a .rvmrc file in a directory after changing to it, if it exists.
   # To disable this fature, set rvm_project_rvmrc=0 in $HOME/.rvmrc
 
   if test "$rvm_project_rvmrc" != 0
     set -l cwd $PWD
+
     while true
+
+      # if cwd is any of '' $home '/'
       if contains $cwd "" $HOME "/"
-        if test "$rvm_project_rvmrc_default" = 1
-          rvm default 1>/dev/null 2>&1
-        end
+        rvm default 1>/dev/null 2>&1
         break
+
       else
-        if test -s ".rvmrc"
-          eval "rvm reload" > /dev/null
+        if test -s "$cwd/.rvmrc" -o -s "$cwd/.ruby-version"
+          rvm rvmrc load "$cwd"
           break
         else
           set cwd (dirname "$cwd")
         end
       end
+
     end
 
   end
