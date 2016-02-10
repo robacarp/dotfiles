75ec20f7cfe9a2b83c5d4c8d5bdbe8eb8ba2d0e9
Robert L. Carpenter <robacarp@gmail.com>
2016-02-09 17:07:18 -0700

    minimal lua framework for window management

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,4 @@
+hs.window.animationDuration = 0
 function reloadConfig(files)
   doReload = false
   for _,file in pairs(files) do
@@ -10,73 +11,149 @@ function reloadConfig(files)
     hs.reload()
   end
 end
+hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
 
-function environment()
+-- modalyolo keeps track of bindings and allows us to wrap all events
+ModalYoLo = {}
+function ModalYoLo:new(modal_key)
+  local obby = {
+    binding = hs.hotkey.modal.new('',modal_key)
+  }
+
+  obby.binding:bind('', 'escape', function()
+    obby.binding:exit()
+  end)
+
+  self.__index = self
+  return setmetatable(obby, self)
+end
+
+  function ModalYoLo:bind(key, mutator)
+    self.binding:bind('', key, function()
+      self.binding:exit()
+      mutator(State:new(), Mutation:new())
+    end)
+  end
+
+-- a mutation object is what allows chainable transforms
+Mutation = {}
+function Mutation:new()
   local win = hs.window.focusedWindow()
   local scr = win:screen()
 
-  return {
-    window = win,
-    frame = win:frame(),
-    screen = win:screen(),
-    screen_size = scr:frame()
+  local obby = {
+    frame_x = 0,
+    frame_y = 0,
+    frame_w = 1000,
+    frame_h = 1000,
+    frame   = win:frame(),
+    screen  = scr,
+    window  = win
   }
+
+  self.__index = self
+  return setmetatable(obby, self)
 end
 
-hs.window.animationDuration = 0
-hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
-hs.alert.show('Config loaded')
+  function Mutation:debug()
+    hs.alert.show( table.tostring( 
+    self
+    ), 6 )
+  end
 
-activation_key = hs.hotkey.modal.new('', 'f6')
-activation_key:bind('', 'escape', function()
-  activation_key:exit()
+  function Mutation:x(ex)
+    self.frame_x = ex
+    return self
+  end
+
+  function Mutation:y(ey)
+    self.frame_y = ey
+    return self
+  end
+
+  function Mutation:h(eh)
+    self.frame_h = eh
+    return self
+  end
+
+  function Mutation:w(ew)
+    self.frame_w = ew
+    return self
+  end
+
+  function Mutation:commit()
+    self.frame.x = self.frame_x
+    self.frame.y = self.frame_y
+    self.frame.w = self.frame_w
+    self.frame.h = self.frame_h
+
+    self.window:setFrame(self.frame)
+  end
+
+
+-- State objects are constructed before each key responder
+State = {}
+function State:new()
+  local win = hs.window.focusedWindow()
+  local scr = win:screen()
+
+  local obby = {
+    frame_x = win:frame().x,
+    frame_y = win:frame().y,
+    frame_h = win:frame().h,
+    frame_w = win:frame().w,
+
+    scr_half_w  = scr:frame().w / 2,
+    scr_half_h  = scr:frame().h / 2,
+
+    scr_w   = scr:frame().w,
+    scr_h   = scr:frame().h
+  }
+
+  self.__index = self
+  return setmetatable(obby, self)
+end
+
+
+-- Main modal object
+
+modal = ModalYoLo:new('f6')
+
+-- Left and right columns (80% and full)
+
+modal:bind('0', function(s,mutator)
+  mutator:x(s.scr_half_w):y(0):w(s.scr_half_w):h(s.scr_h * 0.8):commit()
 end)
 
-activation_key:bind('','9', function()
-  local env = environment()
-
-  env.frame.x = 0
-  env.frame.y = 0
-  env.frame.w = env.screen_size.w / 2
-  env.frame.h = env.screen_size.h * 0.85
-
-  env.window:setFrame(env.frame)
-  activation_key:exit()
+modal:bind('9', function(s,mutator)
+  mutator:x(0):y(0):w(s.scr_half_w):h(s.scr_h * 0.8):commit()
 end)
 
-activation_key:bind('','0', function()
-  local env = environment()
-
-  env.frame.x = env.screen_size.w / 2
-  env.frame.y = 0
-  env.frame.w = env.screen_size.w / 2
-  env.frame.h = env.screen_size.h * 0.85
-
-  env.window:setFrame(env.frame)
-  activation_key:exit()
+modal:bind('r', function(s, mutator)
+  mutator:x(0):y(0):w(s.scr_half_w):h(s.scr_h):commit()
 end)
 
-activation_key:bind('','r', function()
-  local env = environment()
-
-  env.frame.x = 0
-  env.frame.y = 0
-  env.frame.w = env.screen_size.w / 2
-  env.frame.h = env.screen_size.h
-
-  env.window:setFrame(env.frame)
-  activation_key:exit()
+modal:bind('l', function(s, mutator)
+  mutator:x(s.scr_half_w):y(0):w(s.scr_half_w):h(s.scr_h):commit()
 end)
 
-activation_key:bind('','l', function()
-  local env = environment()
+-- 4 corners grid view
 
-  env.frame.x = env.screen_size.w / 2
-  env.frame.y = 0
-  env.frame.w = env.screen_size.w / 2
-  env.frame.h = env.screen_size.h
-
-  env.window:setFrame(env.frame)
-  activation_key:exit()
+modal:bind('1', function(s, mutator)
+  mutator:x(0):y(0):w(s.scr_half_w):h(s.scr_half_h):commit()
 end)
+
+modal:bind('2', function(s, mutator)
+  mutator:x(s.scr_half_w):y(0):w(s.scr_half_w):h(s.scr_half_h):commit()
+end)
+
+modal:bind('\'', function(s, mutator)
+  mutator:x(0):y(s.scr_half_h):w(s.scr_half_w):h(s.scr_half_h):commit()
+end)
+
+modal:bind(',', function(s, mutator)
+  mutator:x(s.scr_half_w):y(s.scr_half_h):w(s.scr_half_w):h(s.scr_half_h):commit()
+end)
+
+hs.alert.show('Config loaded', 1)
 
