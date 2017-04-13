5d0da497b3df370c91815894e7912710d83fde69
Robert L Carpenter <robert@robacarp.com>
2017-04-13 14:54:26 -0600

    reconfiguring hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,152 +1,34 @@
+require('anti_rsi_watcher')
+require('window_management')
+require('config_reloader')
+local timerManager = require('timers')
+
+-- dismiss all active notifications
 hs.notify.withdrawAll()
 
-local ModalYoLo = require('yolo')
-
 hs.window.animationDuration = 0
-function reloadConfig(files)
-  doReload = false
-  for _,file in pairs(files) do
-    if file:sub(-4) == '.lua' then
-      doReload = true
-    end
-  end
 
-  if doReload then
-    hs.reload()
-  end
-end
-hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
-
--- from the example, send the clipboard as regular keystrokes
+-- from the online examples, send the clipboard as regular keystrokes
 hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
--- AntiRSI watcher
-local antiRSINotification = nil
-function checkAntiRSI()
-  if hs.application.find('com.onnlucky.antirsi') == nil then
+-- timerManager = timerManager:new()
+-- timerManager:defineTrigger({
+--   application = 'Terminal',
+--   action = nil
+-- })
 
-    if antiRSINotification ~= nil then
-      antiRSINotification:withdraw()
-    end
+local Toggl = require('toggl')
 
-    antiRSINotification = hs.notify.new('AntiRSI launcher',
-      {
-        autoWithdraw = true,
-        title = 'PRACABOR HEAVY INDUSTRIES',
-        subTitle = 'Department of Health and Safety',
-        informativeText = 'AntiRSI is not running',
-        hasActionButton = true,
-        actionButtonTitle = 'Launch!'
-      }
-    ):send()
-  end
-end
+local toggle_api = Toggl:new({
+  -- username = '',
+  -- password = ''
+  api_key = '8d3212a47f2cce22d46300d3ff75c7d6'
+})
 
-local applicationWatcher = hs.timer.new(60 * 15, checkAntiRSI)
-applicationWatcher:start()
-applicationWatcher:setNextTrigger(3)
+-- local api_key = toggle_api:fetch_api_key()
+-- print("API key is " .. api_key)
 
-hs.notify.register('AntiRSI launcher', function()
-  antiRSINotification = nil
-  hs.application.launchOrFocusByBundleID('com.onnlucky.antirsi')
-end)
-
--- Main modal object
-modal = ModalYoLo:new('f6')
-
-
-
--- Left Columns
--- 20% wide
-modal:bind('h', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w * 0.2):h(mutator.screen.h):commit()
-end)
-
--- 50% wide
-modal:bind('g', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w * 0.5):h(mutator.screen.h):commit()
-end)
-
--- 80% wide
-modal:bind('7', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w * 0.8):h(mutator.screen.h):commit()
-end)
-
-
--- Centered Column
--- 20% wide
-modal:bind('t', function(mutator)
-  mutator:x(mutator.screen.w * 0.4):y(0):w(mutator.screen.w * 0.2):h(mutator.screen.h):commit()
-end)
-
--- 50% wide
-modal:bind('c', function(mutator)
-  mutator:x(mutator.screen.w * 0.25):y(0):w(mutator.screen.w * 0.5):h(mutator.screen.h):commit()
-end)
-
--- 80% wide
-modal:bind('8', function(mutator)
-  mutator:x(mutator.screen.w * 0.1):y(0):w(mutator.screen.w * 0.8):h(mutator.screen.h):commit()
-end)
-
-
-
--- Right Columns
--- 20% wide
-modal:bind('n', function(mutator)
-  mutator:x(mutator.screen.w * 0.8):y(0):w(mutator.screen.w * 0.2):h(mutator.screen.h):commit()
-end)
-
--- 50% wide
-modal:bind('r', function(mutator)
-  mutator:x(mutator.screen.w * 0.5):y(0):w(mutator.screen.w * 0.5):h(mutator.screen.h):commit()
-end)
-
--- 80% wide
-modal:bind('9', function(mutator)
-  mutator:x(mutator.screen.w * 0.2):y(0):w(mutator.screen.w * 0.8):h(mutator.screen.h):commit()
-end)
-
-
-
--- 4 corners grid view
--- top left
-modal:bind('1', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w * 0.5):h(mutator.screen.h * 0.5):commit()
-end)
-
--- top right
-modal:bind('2', function(mutator)
-  mutator:x(mutator.screen.w * 0.5):y(0):w(mutator.screen.w * 0.5):h(mutator.screen.h * 0.5):commit()
-end)
-
--- bottom left
-modal:bind('\'', function(mutator)
-  mutator:x(0):y(mutator.screen.h * 0.5):w(mutator.screen.w * 0.5):h(mutator.screen.h * 0.5):commit()
-end)
-
--- bottom right
-modal:bind(',', function(mutator)
-  mutator:x(mutator.screen.w * 0.5):y(mutator.screen.h * 0.5):w(mutator.screen.w * 0.5):h(mutator.screen.h * 0.5):commit()
-end)
-
--- full screen
-modal:bind('5', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w):h(mutator.screen.h):commit()
-end)
-
-
--- Top and bottom split view
--- Top
-modal:bind('d', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w):h(mutator.screen.h * 0.5):commit()
-end)
-
--- Bottom
-modal:bind('b', function(mutator)
-  mutator:x(0):y(mutator.screen.h * 0.5):w(mutator.screen.w):h(mutator.screen.h * 0.5):commit()
-end)
-
-
+workspaces = toggle_api:get_workspaces()
+local inspect = require('downloaded_modules/inspect')
+print("Got " .. #workspaces .. " workspaces")
 hs.alert.show('HammerSpoon Activated.', 1)
-
