22ff8dca05fa11368f4593fa4d2dbc792bd0026f
Robert L Carpenter <robert@robacarp.com>
2018-04-21 21:46:15 -0600

    hammerspoon: convert config reloader into spoon

---
+++ .hammerspoon/Spoons/ConfigReloader.spoon/init.lua
@@ -0,0 +1,40 @@
+local reloader = {}
+reloader.__index = reloader
+
+reloader.name = "Config Reloader"
+reloader.version = "0.1"
+reloader.author = "Robert Carpenter"
+reloader.license = "MIT - https://opensource.org/licenses/MIT"
+
+reloader.watcher = nil
+
+function reloader:init()
+end
+
+function reloader:start()
+  path = os.getenv('HOME') .. '/.hammerspoon/'
+  self.watcher = hs.pathwatcher.new(
+    path,
+
+    function(files, flags)
+      should_reload = false
+      for _,file in pairs(files) do
+        if file:sub(-4) == '.lua' then
+          should_reload = true
+        end
+      end
+
+      if should_reload then
+        hs.reload()
+      end
+    end
+  )
+
+  self.watcher:start()
+end
+
+function reloader:stop()
+  self.watcher:stop()
+end
+
+return reloader
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,5 @@
 require('anti_rsi_watcher')
 require('window_management')
-require('config_reloader')
 local timerManager = require('timers')
 
 -- dismiss all active notifications
@@ -14,24 +13,10 @@ hs.hotkey.bind({"cmd", "alt"}, "V", function()
   hs.eventtap.keyStrokes(hs.pasteboard.getContents())
 end)
 
--- timerManager = timerManager:new()
--- timerManager:defineTrigger({
---   application = 'Terminal',
---   action = nil
--- })
+hs.loadSpoon('ConfigReloader')
+spoon.ConfigReloader:start()
 
-local Toggl = require('toggl')
-
-local toggle_api = Toggl:new({
-  -- username = '',
-  -- password = ''
-  api_key = '8d3212a47f2cce22d46300d3ff75c7d6'
 })
 
--- local api_key = toggle_api:fetch_api_key()
--- print("API key is " .. api_key)
 
-workspaces = toggle_api:get_workspaces()
-local inspect = require('downloaded_modules/inspect')
-print("Got " .. #workspaces .. " workspaces")
 hs.alert.show('HammerSpoon Activated.', 1)
