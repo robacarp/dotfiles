3d17cebaca1a8c841b58770ae17c5ce988478713
Robert L Carpenter <robert@robacarp.com>
2019-04-29 11:20:01 -0600

    dark mode controls via terminal

---
+++ .config/fish/functions/dark_mode.fish
@@ -0,0 +1,3 @@
+function dark_mode
+  osascript -e "tell application \"System Events\" to tell appearance preferences to set dark mode to True"
+end
---
+++ .config/fish/functions/light_mode.fish
@@ -0,0 +1,3 @@
+function light_mode
+  osascript -e "tell application \"System Events\" to tell appearance preferences to set dark mode to False"
+end
