d9e363ee8af2c8d10b0d626724afba650e593850
Robert L Carpenter <robert@robacarp.com>
2019-10-17 12:42:16 -0600

    correctly search for ec2 ip addresses

--- .config/fish/functions/aws_ip.fish
+++ .config/fish/functions/aws_ip.fish
@@ -1,4 +1,4 @@
 function aws_ip
-  aws ec2 describe-instances --filters 'Name=instance-id,Values=i-0b33038d5133264c5' --query "Reservations[0].Instances[0].PublicIpAddress" --output=text
+  aws ec2 describe-instances --filters "Name=instance-id,Values=$argv[1]" --query "Reservations[0].Instances[0].PublicIpAddress" --output=text
   return $status
 end
