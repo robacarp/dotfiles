ec8f83b2a45f2413d25a3ce0aa88732854e11c56
Rob <robacarp@users.noreply.github.com>
2021-01-11 14:30:04 -0700

    update terminal alert for macos 11
    url-escapes spaces to the 'open' call

--- .config/fish/functions/alert.fish
+++ .config/fish/functions/alert.fish
@@ -1,4 +1,5 @@
 function alert
   argparse --name alert 'm/message=' 't/timeout=' 's/status=' -- $argv or return
-  open -g "hammerspoon://task_completed?message=$_flag_message&timeout=$_flag_timeout&status=$_flag_status"
+  set -l formatted_message (echo $_flag_message |  sed -e 's/ /%20/g')
+  open -g "hammerspoon://task_completed?message=$formatted_message&timeout=$_flag_timeout&status=$_flag_status"
 end
