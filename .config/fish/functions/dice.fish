dc594b3fbcc7f3337c13b84af551553afe66d391
Rob <robacarp@users.noreply.github.com>
2021-08-02 16:21:38 -0600

    update diceware generation scripts

--- .config/fish/functions/dice.fish
+++ .config/fish/functions/dice.fish
@@ -1,8 +1,4 @@
 function dice
-  set -l word_list (curl -s https://robacarp.io/dice.json)
-  set -l color (echo "$word_list" | jq -r .colors[] | shuf -n 1)
-  set -l adjective (echo "$word_list" | jq -r .adjectives[] | shuf -n 1)
-  set -l animal (echo "$word_list" | jq -r .nouns[] | shuf -n 1)
-
-  echo "$adjective-$color-$animal"
+  curl -s https://robacarp.io/new_dice.json \
+  | jq --raw-output --exit-status .chosen[0].assembled
 end
--- .config/fish/functions/dice_email.fish
+++ .config/fish/functions/dice_email.fish
@@ -1,5 +1,4 @@
 function dice_email
   set -l dice (dice)
-  set -l number (random 1 99)
-  echo "$dice$number@$argv[1]"
+  echo "$dice@$argv[1]"
 end
